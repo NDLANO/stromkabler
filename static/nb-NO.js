07bc9716090a597d42c0e79b2cff06779f94480a
Roger Hestad <roger@wodger.com>
2018-10-30 15:57:58 +0100

    Adds language files with only headings

---
+++ static/nb-NO.js
@@ -0,0 +1,5 @@
+﻿window.elektrolyseDictionary = {
+  general: {
+    heading: 'Strømkabler',
+  },
+};
---
+++ static/nn-NO.js
@@ -0,0 +1,186 @@
+﻿window.elektrolyseDictionary = {
+  general: {
+    heading: 'Protein og stivelse',
+    correctAnswer: 'Du svarte rett!',
+    wrongAnswer: 'Du svarte ikkje rett...',
+    startButton: 'Start',
+    nextButton: 'Neste',
+    nextFoodItemButton: 'Neste matvare',
+    viewResult: 'Sjå resultat',
+    alt1Button: 'A',
+    alt2Button: 'B',
+    altProteinButton: 'Inneheld protein',
+    altStivelseButton: 'Inneheld stivelse',
+  },
+  menu: {
+    theory: 'Teori',
+    part1: 'Utstyr',
+    part2: 'Bevis',
+    part3: 'Introduksjon',
+    part4: 'Test matvarer',
+    result: 'Resultat',
+  },
+  splash: {
+    title: 'Påvising av protein og stivelse',
+    paragraph1:
+      'Formålet med forsøket er at du skal lære å gjennomføre enkle kjemiske påvisingar av næringsstoffer i matvarer og gjere greie for observasjonane.',
+    cc: 'Bakgrunnsbilete: Public Domain Dedication PIRO4D/Pixabay',
+  },
+  intro: {
+    title: 'Teori',
+    paragraph1:
+      'Vi skal nå gjennomføre ei laboratorieundersøking. Med hjelp av ei koparsulfat-løysning og ei jod-løysning  kan du teste om matvarer inneheld protein og/eller stivelse.',
+    paragraph2:
+      'Vi startar med å teste korleis stivelse og protein reagerer med koparsulfat og jod. Ved å sjå etter fargeforandringar i glasa kan du finne ut kva løysning som blir brukt til å påvise stivelse og kva løysning som kan påvise protein. ',
+  },
+  side3: {
+    paragraph1: 'Her er utstyret du vil bruke i eksperimenta.',
+    jod: 'Jod',
+    kobbersulfat: 'Koparsulfat',
+    dropcounter: 'Dråpetellar',
+    glass: 'Glas',
+    banana: 'Banan',
+    milk: 'Melk',
+    cheese: 'Ost',
+    potato: 'Potet',
+    bread: 'Brød',
+  },
+  side4: {
+    paragraph1:
+      'Vi startar med å teste korleis protein reagerer med koparsulfat og jod. Legg merke til fargeforandringar.',
+    animTitle:
+      'Animasjon som viser korleis protein reagerer med koparsulfat og jod.',
+    animDescription:
+      'Animasjonen viser at når ein tek koparsulfat i protein blir fargen lilla, når ein tek jod i protein får ein ingen reaksjon',
+    altButton: 'Test med protein',
+  },
+  side5: {
+    paragraph1:
+      'Nå skal vi teste korleis stivelse reagerer med koparsulfat og jod. Legg merke til fargeforandringar.',
+    altButton: 'Test med protein',
+    animTitle:
+      'Animasjon som viser eksperiment av korleis stivelse reagerer med koparsulfat og jod.',
+    animDescription:
+      'Animasjonen viser at når ein tek koparsulfat i stivelse får ein ingen reaksjon, men når ein tek jod i stivelse blir den blå/svart',
+  },
+  side6: {
+    question:
+      'La du merke til fargeforandringane når du testa korleis stivelse og protein reagerer med koparsulfat og jod? Kva slags løysning påviser protein, og kva for ein påviser stivelse?',
+    alt1Button: 'Koparsulfat påviser stivelse, jod påviser protein.',
+    alt2Button: 'Koparsulfat påviser protein, jod påviser stivelse.',
+    paragraph1:
+      'Koparsulfat kan bli brukt til å påvise protein, ved at vi ser eit fargeomslag til lilla.',
+    paragraph2:
+      'Jod kan bli brukt til å påvise stivelse, ved at vi ser eit fargeomslag til blå/svart.',
+    altTekstBilde:
+      'Bilde som viser stivelse som reagerer med koparsulfat og jod som reagerer med protein.',
+  },
+  side7: {
+    heading: 'Introduksjon',
+    paragraph1:
+      'Nå skal vi bruke dette til å teste nokre matvarer for stivelse og protein.',
+    paragraph2:
+      'Etter testing får du høve til å krysse av for om matvara inneheld mykje stivelse eller protein.',
+  },
+  side8: {
+    paragraph1:
+      'Test om du får fargeomslag på banan. Hugs at koparsulfat påviser proteiner ved fargeomslag til lilla, og jod påviser stivelse ved fargeomslag til blå/svart.',
+    animTitle: 'Animasjon som viser test av banan',
+    animDescription: 'Animasjonen viser at jod reagerer med banan',
+    altButton: 'Test banan',
+  },
+  side9: {
+    paragraph1:
+      'Test om du får fargeomslag på melk. Hugs at koparsulfat påviser proteiner ved fargeomslag til lilla, og jod påviser stivelse ved fargeomslag til blå/svart.',
+    animTitle: 'Animasjon som viser test av melk',
+    animDescription: 'Animasjonen viser at koparsulfat reagerer med melk',
+    altButton: 'Test melk',
+  },
+  side10: {
+    paragraph1:
+      'Test om du får fargeomslag på ost. Hugs at koparsulfat påviser proteiner ved fargeomslag til lilla, og jod påviser stivelse ved fargeomslag til blå/svart.',
+    animTitle: 'Animasjon som viser test av ost',
+    animDescription: 'Animasjonen viser at koparsulfat reagerer med ost',
+    altButton: 'Test ost',
+  },
+  side11: {
+    paragraph1:
+      'Test om du får fargeomslag på potet. Hugs at koparsulfat påviser proteiner ved fargeomslag til lilla, og jod påviser stivelse ved fargeomslag til blå/svart.',
+    animTitle: 'Animasjon som viser test av potet',
+    animDescription: 'Animasjonen viser at jod reagerer med potet',
+    altButton: 'Test potet',
+  },
+  side12: {
+    paragraph1:
+      'Test om du får fargeomslag på brød. Hugs at koparsulfat påviser proteiner ved fargeomslag til lilla, og jod påviser stivelse ved fargeomslag til blå/svart.',
+    animTitle: 'Animasjon som viser test av brød',
+    animDescription:
+      'Animasjonen viser at både koparsulfat og jod reagerer med brød',
+    altButton: 'Test brød',
+  },
+  result: {
+    paragraph1:
+      'Vi kan bruke koparsulfat til å påvise protein, ved at vi ser eit fargeomslag til lilla.<br>Vi kan bruke jod til å påvise stivelse, ved at vi ser eit fargeomslag til blå/svart.',
+    result: 'Resultat',
+    correct: 'Rett',
+    wrong: 'Feil',
+    showFasit: 'Sjekk svara dine',
+    test: [
+      {
+        testObjectName: 'Banan',
+        stivelse: {
+          answer: null,
+          fasit: true,
+        },
+        protein: {
+          answer: null,
+          fasit: false,
+        },
+      },
+      {
+        testObjectName: 'Melk',
+        stivelse: {
+          answer: null,
+          fasit: false,
+        },
+        protein: {
+          answer: null,
+          fasit: true,
+        },
+      },
+      {
+        testObjectName: 'Ost',
+        stivelse: {
+          answer: null,
+          fasit: false,
+        },
+        protein: {
+          answer: null,
+          fasit: true,
+        },
+      },
+      {
+        testObjectName: 'Potet',
+        stivelse: {
+          answer: null,
+          fasit: true,
+        },
+        protein: {
+          answer: null,
+          fasit: false,
+        },
+      },
+      {
+        testObjectName: 'Brød',
+        stivelse: {
+          answer: null,
+          fasit: true,
+        },
+        protein: {
+          answer: null,
+          fasit: true,
+        },
+      },
+    ],
+  },
+};
