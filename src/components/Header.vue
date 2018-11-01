58f48d7d1dabe9fc7a420efe54644b02e6a5a24a
Roger Hestad <roger@wodger.com>
2018-11-01 15:24:37 +0100

    Header: Comments out menu button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
   header
-      button.btn-menu(@click="toggleMobileMenu")
+      //- button.btn-menu(@click="toggleMobileMenu")
         <svg-icon name="icon-menu" size="32"></svg-icon>
       h1.header_title {{ $t('general.heading')}}             
 </template>
 
 <script>
-export default {
-  methods: {
-    toggleMobileMenu() {
-      console.log('Header.toggleMobileMenu');
-      this.$emit('toggleMobileMenu');
+  export default {
+    methods: {
+      toggleMobileMenu() {
+        console.log('Header.toggleMobileMenu');
+        this.$emit('toggleMobileMenu');
+      },
     },
-  },
-};
+  };
 </script>
 
 <style lang="scss">
