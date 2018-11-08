43ea74f9d81621714d2cd53e38bfb71223ae6d29
Roger Hestad <roger@wodger.com>
2018-11-08 15:29:14 +0100

    Illustration: Text for sikringsskap adjusted and variable added

--- src/components/Illustration.vue
+++ src/components/Illustration.vue
@@ -1,9 +1,31 @@
 <template src="./Illustration.html"></template>
 
+<script>
+  export default {
+    data() {
+      return {
+        u1: '230',
+      };
+    },
+  };
+</script>
+
+
 <style lang="scss" scoped>
   .ill-text {
-    font-size: 150px;
     font-weight: bold;
+    font-size: 150px;
   }
+  .ill-text-sikringsskap {
+    font-size: 110px;
+
+    .sub {
+      font-size: 65px;
+    }
+  }
+
+  // .ill-text-length {
+  //   font-size: 150px;
+  // }
 </style>
 
